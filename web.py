dc3df2613c0d273415e65253acb9570decca622e
mansi <mansikothari46@gmail.com>
2023-05-21 04:46:01 -0700

    finally done

--- web.py
+++ web.py
@@ -8,6 +8,8 @@ create_file(FILEPATH)
 
 todos = get_todos(FILEPATH)
 
+#st.set_page_config(layout='wide')
+
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
@@ -17,8 +19,8 @@ def add_todo():
 
 st.title("My ToDo App")
 st.subheader("This is My Todo App")
-st.write("This app is for you to help record of things to do.")
-
+st.write("This app is for you to help <b>record</b> of things to do.",)
+#For html use unsafe_allow_html=True in st.write
 for index, todo in enumerate(todos):
     Checkbox = st.checkbox(todo, key=todo)
     if Checkbox:
